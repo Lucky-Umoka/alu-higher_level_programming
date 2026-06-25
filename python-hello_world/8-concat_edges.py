2dfbedb7db789d8be36894debf7357812617f18b
Lucky Umoka <umokalucky@gmail.com>
2026-06-25 21:32:33 +0000

    Fix print_number, concat and concat_edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python"
-word2 = "is cool!"
-word3 = "object-oriented programming with Python"
-print(word3)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[26:74])
