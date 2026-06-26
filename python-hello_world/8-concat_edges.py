5ba8b2e6d61e5fb2fb06062bf008e5b9b1373351
Lucky Umoka <umokalucky@gmail.com>
2026-06-26 00:14:08 +0000

    let's see

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "object-oriented programming "
-str2 = "with Python"
-print("{}{}".format(str1, str2))
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[106:112] + str[:6]
+print(str)
