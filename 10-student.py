fe569bb6d62093a6ebcec1439dcfe9e80eee4c40
Lucky Umoka <umokalucky@gmail.com>
2026-07-24 22:48:09 +0000

    Rewrite 10-student and 11-student to fully avoid E127

--- 10-student.py
+++ 10-student.py
@@ -23,9 +23,11 @@ class Student:
         Args:
             attrs (list): Optional list of attribute names to filter by.
         """
-        valid = isinstance(attrs, list)
-        if valid:
-            valid = all(isinstance(a, str) for a in attrs)
-        if valid:
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
-        return self.__dict__
+        d = self.__dict__
+        if type(attrs) is list:
+            new_d = {}
+            for k in d:
+                if k in attrs:
+                    new_d[k] = d[k]
+            return new_d
+        return d
--- 11-student.py
+++ 11-student.py
@@ -23,12 +23,14 @@ class Student:
         Args:
             attrs (list): Optional list of attribute names to filter by.
         """
-        valid = isinstance(attrs, list)
-        if valid:
-            valid = all(isinstance(a, str) for a in attrs)
-        if valid:
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
-        return self.__dict__
+        d = self.__dict__
+        if type(attrs) is list:
+            new_d = {}
+            for k in d:
+                if k in attrs:
+                    new_d[k] = d[k]
+            return new_d
+        return d
 
     def reload_from_json(self, json):
         """Replace all attributes of the Student instance from a dict.
