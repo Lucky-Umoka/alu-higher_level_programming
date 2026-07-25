967c3144f0775e42622ac7cc38cdab1d0a9785b1
Lucky Umoka <umokalucky@gmail.com>
2026-07-25 02:26:38 +0000

    Fix case-sensitivity in 2-my_filter_states and 13-model_state_delete_a

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%')).all()
+    states = session.query(State).filter(
+        State.name.op('LIKE BINARY')('%a%')).all()
     for state in states:
         session.delete(state)
     session.commit()
