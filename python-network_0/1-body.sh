3ea985c8e493fe158c8fe4ddf390555f65c8f344
Lucky Umoka <umokalucky@gmail.com>
2026-07-25 00:49:00 +0000

    Fix 1-body.sh redirect handling and 4-header.sh header name

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# displays the body of a GET response only if status code is 200
-curl -s -o /tmp/body.txt -w "%{http_code}" "$1" | grep -q "^200$" && cat /tmp/body.txt
+# displays the body of a GET response only if final status code is 200
+curl -s -L -o /tmp/body.txt -w "%{http_code}" "$1" | grep -q "^200$" && cat /tmp/body.txt
