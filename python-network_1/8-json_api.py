b9f1c0dcd2926ff50bc521b122d6303dfc83748a
Lucky Umoka <umokalucky@gmail.com>
2026-07-25 00:53:35 +0000

    Add python-network_1 tasks

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that searches a user via a POST request and parses JSON."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    r = requests.post("http://0.0.0.0:5000/search_user", data={"q": letter})
+    try:
+        result = r.json()
+    except ValueError:
+        print("Not a valid JSON")
+    else:
+        if not result:
+            print("No result")
+        else:
+            print("[{}] {}".format(result.get("id"), result.get("name")))
